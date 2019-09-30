d18a939186c740a8936b89283361975d40c85577
snpdolan <snpdolan@gmail.com>
2019-09-29 22:32:20 -0400

    added basic eslint files and sketch of the Notifications service.

---
+++ src/services/notifications.js
@@ -0,0 +1,24 @@
+function notify() {
+  // Let's check if the browser supports notifications
+  if (!("Notification" in window)) {
+    console.log("This browser does not support system notifications");
+  }
+
+  // Let's check whether notification permissions have already been granted
+  else if (Notification.permission === "granted") {
+    // If it's okay let's create a notification
+    var notification = new Notification("Hi there!");
+  }
+
+  // Otherwise, we need to ask the user for permission
+  else if (Notification.permission !== 'denied') {
+    Notification.requestPermission(function (permission) {
+      // If the user accepts, let's create a notification
+      if (permission === "granted") {
+        //var notification = new Notification("Hi there!");
+      }
+    });
+  }
+}
+
+module.exports = notify;
